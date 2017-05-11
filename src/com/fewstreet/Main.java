7d8de53498252cfc9b21fa61fbf54203a42726b2
Peter Den Hartog <pddenhar@gmail.com>
2017-05-10 20:53:33 -0500

    Migrate all media files

--- src/com/fewstreet/Main.java
+++ src/com/fewstreet/Main.java
@@ -1,9 +1,8 @@
 package com.fewstreet;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+
+import static com.fewstreet.MediaFile.VERSION;
 
 public class Main {
 
@@ -11,19 +10,52 @@ public class Main {
         MediaFileMapper m = new MediaFileMapper();
         try {
             Connection hsqldbc = DriverManager.getConnection("jdbc:hsqldb:file:C:\\Users\\Peter\\Downloads\\db\\subsonic", "SA", "");
-            //Connection mysqlc = DriverManager.getConnection("jdbc:mysql://localhost/subsonic", "subsonic", "");
-            ResultSet r = hsqldbc.createStatement().executeQuery("SELECT * from MEDIA_FILE ORDER BY id ASC LIMIT 2000");
+            Connection mysqlc = DriverManager.getConnection("jdbc:mysql://192.168.1.221/subsonic_migrate?useJDBCCompliantTimezoneShift=true&serverTimezone=UTC&useSSL=false", "subsonic", "xjXtvllUcRN664pP");
+            ResultSet r = hsqldbc.createStatement().executeQuery("SELECT * from MEDIA_FILE ORDER BY id ASC");
             int i = 0;
-            while(r.next() && i < 700){
-                MediaFile mf = m.mapRow(r);
-                System.out.println(mf.id +" "+mf.artist);
+            mysqlc.setAutoCommit(false);
+            while(r.next()){
+                MediaFile file = m.mapRow(r);
+                //System.out.println(mf.id +" "+mf.artist);
+                insert(mysqlc, "insert into media_file (" + MediaFile.INSERT_COLUMNS + ") values (" + questionMarks(MediaFile.INSERT_COLUMNS) + ")",
+                        file.getId(), file.getPath(), file.getFolder(), file.getMediaType().name(), file.getFormat(), file.getTitle(), file.getAlbumName(), file.getArtist(),
+                        file.getAlbumArtist(), file.getDiscNumber(), file.getTrackNumber(), file.getYear(), file.getGenre(), file.getBitRate(),
+                        file.isVariableBitRate(), file.getDurationSeconds(), file.getFileSize(), file.getWidth(), file.getHeight(),
+                        file.getCoverArtPath(), file.getParentPath(), file.getPlayCount(), file.getLastPlayed(), file.getComment(),
+                        file.getCreated(), file.getChanged(), file.getLastScanned(),
+                        file.getChildrenLastUpdated(), file.isPresent(), VERSION);
                 i++;
             }
+            System.out.println("Inserted all media files");
+            mysqlc.commit();
+            mysqlc.close();
             hsqldbc.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
+    }
+
+    protected static String questionMarks(String columns) {
+        int count = columns.split(", ").length;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            builder.append('?');
+            if (i < count - 1) {
+                builder.append(", ");
+            }
+        }
+        return builder.toString();
+    }
+
+    private static int insert(Connection conn, String query, Object ... args) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement(query);
+        for (int i = 0; i < args.length; i++) {
+            stmt.setObject(i+1, args[i]);
+        }
+        int result = stmt.executeUpdate();
+        stmt.close();
+        return result;
     }
 }
 
--- src/com/fewstreet/MediaFile.java
+++ src/com/fewstreet/MediaFile.java
@@ -1,44 +1,54 @@
 package com.fewstreet;
 
+
+
+import java.io.File;
 import java.util.Date;
+import java.util.List;
+import org.apache.commons.io.FilenameUtils;
 
 /**
  * Created by peter on 5/10/17.
  */
 
 public class MediaFile {
+    public static final String INSERT_COLUMNS = "id, path, folder, type, format, title, album, artist, album_artist, disc_number, " +
+            "track_number, year, genre, bit_rate, variable_bit_rate, duration_seconds, file_size, width, height, cover_art_path, " +
+            "parent_path, play_count, last_played, comment, created, changed, last_scanned, children_last_updated, present, version";
+    public static final int VERSION = 4;
 
-    public int id;
-    public String path;
-    public String folder;
-    public MediaType mediaType;
-    public String format;
-    public String title;
-    public String albumName;
-    public String artist;
-    public String albumArtist;
-    public Integer discNumber;
-    public Integer trackNumber;
-    public Integer year;
-    public String genre;
-    public Integer bitRate;
-    public boolean variableBitRate;
-    public Integer durationSeconds;
-    public Long fileSize;
-    public Integer width;
-    public Integer height;
-    public String coverArtPath;
-    public String parentPath;
-    public int playCount;
-    public Date lastPlayed;
-    public String comment;
-    public Date created;
-    public Date changed;
-    public Date lastScanned;
-    public Date starredDate;
-    public Date childrenLastUpdated;
-    public boolean present;
-    public int version;
+
+    private int id;
+    private String path;
+    private String folder;
+    private MediaType mediaType;
+    private String format;
+    private String title;
+    private String albumName;
+    private String artist;
+    private String albumArtist;
+    private Integer discNumber;
+    private Integer trackNumber;
+    private Integer year;
+    private String genre;
+    private Integer bitRate;
+    private boolean variableBitRate;
+    private Integer durationSeconds;
+    private Long fileSize;
+    private Integer width;
+    private Integer height;
+    private String coverArtPath;
+    private String parentPath;
+    private int playCount;
+    private Date lastPlayed;
+    private String comment;
+    private Date created;
+    private Date changed;
+    private Date lastScanned;
+    private Date starredDate;
+    private Date childrenLastUpdated;
+    private boolean present;
+    private int version;
 
     public MediaFile(int id, String path, String folder, MediaType mediaType, String format, String title,
                      String albumName, String artist, String albumArtist, Integer discNumber, Integer trackNumber, Integer year, String genre, Integer bitRate,
@@ -79,6 +89,343 @@ public class MediaFile {
 
     public MediaFile() {
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getFolder() {
+        return folder;
+    }
+
+    public void setFolder(String folder) {
+        this.folder = folder;
+    }
+
+    public File getFile() {
+        // TODO: Optimize
+        return new File(path);
+    }
+
+    public MediaType getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(MediaType mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public boolean isVideo() {
+        return mediaType == MediaType.VIDEO;
+    }
+
+    public boolean isAudio() {
+        return mediaType == MediaType.MUSIC || mediaType == MediaType.AUDIOBOOK || mediaType == MediaType.PODCAST;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public boolean isDirectory() {
+        return !isFile();
+    }
+
+    public boolean isFile() {
+        return mediaType != MediaType.DIRECTORY && mediaType != MediaType.ALBUM;
+    }
+
+    public boolean isAlbum() {
+        return mediaType == MediaType.ALBUM;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String album) {
+        this.albumName = album;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbumArtist() {
+        return albumArtist;
+    }
+
+    public void setAlbumArtist(String albumArtist) {
+        this.albumArtist = albumArtist;
+    }
+
+    public String getName() {
+        if (isFile()) {
+            return title != null ? title : FilenameUtils.getBaseName(path);
+        }
+
+        return FilenameUtils.getName(path);
+    }
+
+    public Integer getDiscNumber() {
+        return discNumber;
+    }
+
+    public void setDiscNumber(Integer discNumber) {
+        this.discNumber = discNumber;
+    }
+
+    public Integer getTrackNumber() {
+        return trackNumber;
+    }
+
+    public void setTrackNumber(Integer trackNumber) {
+        this.trackNumber = trackNumber;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Integer getBitRate() {
+        return bitRate;
+    }
+
+    public void setBitRate(Integer bitRate) {
+        this.bitRate = bitRate;
+    }
+
+    public boolean isVariableBitRate() {
+        return variableBitRate;
+    }
+
+    public void setVariableBitRate(boolean variableBitRate) {
+        this.variableBitRate = variableBitRate;
+    }
+
+    public Integer getDurationSeconds() {
+        return durationSeconds;
+    }
+
+    public void setDurationSeconds(Integer durationSeconds) {
+        this.durationSeconds = durationSeconds;
+    }
+
+    public String getDurationString() {
+        if (durationSeconds == null) {
+            return null;
+        }
+
+        StringBuilder result = new StringBuilder(8);
+
+        int seconds = durationSeconds;
+
+        int hours = seconds / 3600;
+        seconds -= hours * 3600;
+
+        int minutes = seconds / 60;
+        seconds -= minutes * 60;
+
+        if (hours > 0) {
+            result.append(hours).append(':');
+            if (minutes < 10) {
+                result.append('0');
+            }
+        }
+
+        result.append(minutes).append(':');
+        if (seconds < 10) {
+            result.append('0');
+        }
+        result.append(seconds);
+
+        return result.toString();
+    }
+
+    public Long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(Long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public String getCoverArtPath() {
+        return coverArtPath;
+    }
+
+    public void setCoverArtPath(String coverArtPath) {
+        this.coverArtPath = coverArtPath;
+    }
+
+
+    public String getParentPath() {
+        return parentPath;
+    }
+
+    public void setParentPath(String parentPath) {
+        this.parentPath = parentPath;
+    }
+
+    public File getParentFile() {
+        return getFile().getParentFile();
+    }
+
+    public int getPlayCount() {
+        return playCount;
+    }
+
+    public void setPlayCount(int playCount) {
+        this.playCount = playCount;
+    }
+
+    public Date getLastPlayed() {
+        return lastPlayed;
+    }
+
+    public void setLastPlayed(Date lastPlayed) {
+        this.lastPlayed = lastPlayed;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getChanged() {
+        return changed;
+    }
+
+    public void setChanged(Date changed) {
+        this.changed = changed;
+    }
+
+    public Date getLastScanned() {
+        return lastScanned;
+    }
+
+    public void setLastScanned(Date lastScanned) {
+        this.lastScanned = lastScanned;
+    }
+
+    public Date getStarredDate() {
+        return starredDate;
+    }
+
+    public void setStarredDate(Date starredDate) {
+        this.starredDate = starredDate;
+    }
+
+    /**
+     * Returns when the children was last updated in the database.
+     */
+    public Date getChildrenLastUpdated() {
+        return childrenLastUpdated;
+    }
+
+    public void setChildrenLastUpdated(Date childrenLastUpdated) {
+        this.childrenLastUpdated = childrenLastUpdated;
+    }
+
+    public boolean isPresent() {
+        return present;
+    }
+
+    public void setPresent(boolean present) {
+        this.present = present;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof MediaFile && ((MediaFile) o).path.equals(path);
+    }
+
+    @Override
+    public int hashCode() {
+        return path.hashCode();
+    }
+
+    public File getCoverArtFile() {
+        // TODO: Optimize
+        return coverArtPath == null ? null : new File(coverArtPath);
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+
 
     public static enum MediaType {
         MUSIC,
